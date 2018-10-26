cb66bfbf8e0a5920fa04815d9af1488ca8479330
Kaeleb <kaelebdrew@gmail.com>
2018-10-26 15:54:17 -0700

    functions complete

--- functions.php
+++ functions.php
@@ -19,7 +19,35 @@
         echo 'Hello '. $name;
     }
 
-    sayHello('Drew'. ' Beaupre');
+    // sayHello('Drew'. ' Beaupre');
+    
+    // Return Value
+    function addNumbers($num1, $num2){
+        return $num1 + $num2;
+    }
+
+    // echo addNumbers(2,3);
+
+    // passing arguments by reference
+
+    $myNum = 10;
+
+    function addFive($num){
+        $num += 5;
+    }
+
+    function addTen(&$num){
+        $num += 10;
+    }
+
+    addFive($myNum);
+
+    echo "Value: $myNum<br>";
+
+    addTen($myNum);
+
+    echo "Value: $myNum<br>";
+    
     
 
 
